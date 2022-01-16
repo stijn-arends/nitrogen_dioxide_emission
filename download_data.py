a278cb9daff69c1273bca0f0731206aa6fc5e350
stijn-arends <stijnarends@live.nl>
2022-01-16 19:43:10 +0100

    Added a python file that will download the data.

---
+++ download_data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# IMPORTS
+import sys
+
+__author__ = "Stijn Arends"
+__date__ = "16-01-2022"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
