f4a4d0c34de6dd4c88b05496d317d8c0b420b2df
stijn-arends <stijnarends@live.nl>
2022-01-16 19:44:20 +0100

    Added a function that extraxts the content of the config file.

--- download_data.py
+++ download_data.py
@@ -2,13 +2,31 @@
 
 # IMPORTS
 import sys
+import yaml
 
 __author__ = "Stijn Arends"
 __date__ = "16-01-2022"
 
 
+
+def get_config() -> dict:
+    """
+    Read in config file and return it as a dictionary.
+    
+    :returns
+    --------
+    config - dict
+        Configuration file in dictionary form.
+    """
+    with open("config.yaml", 'r') as stream:
+        config = yaml.safe_load(stream)
+    return config
+
+
 def main():
-    pass
+    config = get_config()
+    data_dir = config['datadir']
+    urls = config['urls']
 
 
 if __name__ == "__main__":
