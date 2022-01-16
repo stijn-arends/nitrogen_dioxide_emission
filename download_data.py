5634cffadec1e85d4f806e01f0b01e90fe13a0bf
stijn-arends <stijnarends@live.nl>
2022-01-16 19:46:31 +0100

    Created a class that will handle downloading the data. Added a method that creates a directory for the data.

--- download_data.py
+++ download_data.py
@@ -3,10 +3,48 @@
 # IMPORTS
 import sys
 import yaml
+from pathlib import Path
 
 __author__ = "Stijn Arends"
 __date__ = "16-01-2022"
 
+
+class GetData:
+    """
+    A class to download data.
+
+    ...
+
+    Attributes
+    ----------
+    data_dir : str
+        Location of data storage
+
+
+    Methods
+    -------
+    make_data_dir():
+        Create a directory in the system to store the data, if it doesn't exist yet.
+    """
+
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def make_data_dir(self) -> None:
+        """
+        Create a directory (if it does not exisit yet) to store the 
+        data.
+
+        :Excepts
+        --------
+        FileExistsError
+            The directory already exists
+        """
+        path = Path(self.data_dir)
+        try:
+            path.mkdir(parents=True, exist_ok=False)
+        except FileExistsError:
+            print(f"[{self.make_data_dir.__name__}] Folder is already there.")
 
 
 def get_config() -> dict:
