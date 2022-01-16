bc7e1ee45cc021c357ee8816fe46985c95ea1b5e
stijn-arends <stijnarends@live.nl>
2022-01-16 20:13:50 +0100

    Added a function (log(logger)) that acts as a decorator for other functions and writes out information to a log file.

--- logger.py
+++ logger.py
@@ -40,6 +40,7 @@ def create_logger(file_name):
     :see also
     ---------
     make_dir(folder)
+    log(logger)
     """ 
     #create a logger object
     logger = logging.getLogger('logger')
@@ -60,3 +61,50 @@ def create_logger(file_name):
     logger.addHandler(logfile)
       
     return logger
+
+def log(logger):
+    """
+    Base function for the decorator.
+    Log basic information about a function plus the catched exceptions
+    into a log file.
+
+    :parameters
+    -----------
+    logger - Logger
+        Logging object
+    """
+    def decorator(func):
+          
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            """
+            Store information about the function that is being run and 
+            log it into a log file. Also catch the exceptions that might 
+            occure and write them to the log file aswell. 
+            """
+            start = datetime.datetime.now()
+            fun_name = func.__name__
+            end = datetime.datetime.now()
+              
+            try:
+                text = "no exceptions raised."
+                text += """
+                Function: {}
+                Execution time: {}
+                Adress: {}
+                Memory: {} bytes
+                Date: {}
+                """.format(fun_name, end-start, func.__name__,
+                sys.getsizeof(func), start)
+                logger.info(text)
+                return func(*args, **kwargs)
+                
+            except:
+                issue = "exception in "+func.__name__+"\n"
+                issue = issue+"-------------------------\
+                ------------------------------------------------\n"
+                logger.exception(issue)
+            raise Exception
+               
+        return wrapper
+    return decorator
